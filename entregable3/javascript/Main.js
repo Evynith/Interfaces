79fbc24f44b73e11d591a63bb98c75e7f5e556df
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2020-10-18 21:04:17 -0300

    pikachu 60%

--- entregable3/javascript/Main.js
+++ entregable3/javascript/Main.js
@@ -2,9 +2,11 @@ let card = document.querySelector("#card");
 let coordXa = 0;
 let coordXb = 0;
 let pika =  document.querySelector(".pikachu");
+let corriendo = false; //controla que no quiera animarse si ya hay uno corriendo en ese mismo div
 
 
 function pDerecha(actual, posicion){
+    corriendo = true;
     pika.classList.remove("GizquierdaF");
     pika.classList.remove("Cizquierda");
     pika.classList.remove("GderechaF");
@@ -13,8 +15,8 @@ function pDerecha(actual, posicion){
         
         let pikaCorreDerecha = pika.animate([
             // keyframes
-            {   left: `${actual}px`, transform : /* "translate(0) */ "scaleX(-1)" }, //porque se sobreescribe el transform
-            {   left: `${posicion}px`,  transform : /* `translate(${posicion}px) */ `scaleX(-1)` }
+            {   left: `${actual}px`,/*  transform :  "translate(0) scaleX(-1)" */ }, //porque se sobreescribe el transform
+            {   left: `${posicion}px`/* ,  transform :  `translate(${posicion}px) scaleX(-1)` */ }
             ], { 
             // timing options
                 duration: 1000,
@@ -29,10 +31,12 @@ function pDerecha(actual, posicion){
             // pika.style.left = pika.getBoundingClientRect().left + (posicion - pika.getBoundingClientRect().left);
             pika.classList.remove("Cderecha");
             pika.classList.add("GderechaF");
+            setTimeout(function () { corriendo = false; }, 1000); //porque los girando duran 1s
         });
     }, 1000);
 }
 function pIzquierda(actual, posicion){
+    corriendo = true;
     pika.classList.remove("GderechaF");
     pika.classList.remove("Cderecha");
     pika.classList.remove("GizquierdaF");
@@ -57,40 +61,48 @@ function pIzquierda(actual, posicion){
             // pika.style.left = pika.getBoundingClientRect().left + (posicion - pika.getBoundingClientRect().left);
             pika.classList.remove("Cizquierda");
             pika.classList.add("GizquierdaF");
+            setTimeout(function () { corriendo = false; }, 1000); //porque los girando duran 1s
         });
     }, 1000);
 }
 
-function limpiarAnimacionPika(){
-    pika.getAnimations().forEach(
-        animation => animation.pause() //para que no se sature la memoria
-    );
-}
+// function limpiarAnimacionPika(){
+//     pika.getAnimations().forEach(
+//         animation => animation.pause() //para que no se sature la memoria
+//     );
+// }
 
 card.onmouseover = () => {
     document.onmousemove = (e) => {
         
-        let posActual = pika.getBoundingClientRect().left;
-        console.log(posActual);
-        if(coordXa == 0) {coordXa = e.screenX}
-        coordXb = e.screenX;
+        // let posActual = pika.getBoundingClientRect().left;
+        // console.log(posActual);
+        // if(coordXa == 0) {coordXa = e.screenX}
+        // coordXb = e.screenX;
 
-        if(coordXa < coordXb){
-            //animacion a la derecha
-            pDerecha(posActual, posActual +100);
-        }else if(coordXa >= coordXb){
-            //animacion a la izquierda
-            pIzquierda(posActual, posActual -100);
-        }
-        coordXa = coordXb;
+        // if(coordXa < coordXb){
+        //     //animacion a la derecha
+        //     console.log("derecha");
+        //     pDerecha(posActual, posActual +100);
+        // }else if(coordXa >= coordXb){
+        //     //animacion a la izquierda
+        //     console.log("izquierda");
+        //     pIzquierda(posActual, posActual -100);
+        // }
+        // coordXa = coordXb;
         // limpiarAnimacionPika();
 
         if (  (e.screenX > (screen.width/2 +50)) )  { 
             document.querySelector(".edificios-derecha").style.transform = `rotateY(${-38}deg) `;
-            document.querySelector(".edificios-izquierda").style.transform = `rotateY(${-0.7}deg) `; 
+            document.querySelector(".edificios-izquierda").style.transform = `rotateY(${-0.7}deg) `;
+            if ((corriendo == false) && (Math.round(pika.getBoundingClientRect().left) != 1000)) {
+                pDerecha(200, 1000);
+            };
         } else if ( (e.screenX < (screen.width/2 -50)) ) { 
             document.querySelector(".edificios-derecha").style.transform = `rotateY(${-0.7}deg) `; 
             document.querySelector(".edificios-izquierda").style.transform = `rotateY(${-38}deg) `;
+            if ((corriendo == false) && (Math.round(pika.getBoundingClientRect().left) != 200)) {
+                pIzquierda(1000, 200)};
          } 
     }
 }
