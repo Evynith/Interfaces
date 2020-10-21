397c5e8bcf15a89ac36b49cb37a66a5ec9fc2085
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2020-10-21 00:42:08 -0300

    sin imagenes para que no de error git

--- entregable3/javascript/Main.js
+++ entregable3/javascript/Main.js
@@ -4,6 +4,8 @@ let coordXb = 0;
 let pika =  document.querySelector(".pikachu");
 let corriendo = false; //controla que no quiera animarse si ya hay uno corriendo en ese mismo div
 
+
+//_____________________________________________________________________________________ANIMACION PIKACHU HOME
 function pDerecha(actual, posicion){
     corriendo = true;
     pika.classList.remove("GizquierdaF");
@@ -59,6 +61,8 @@ function pIzquierda(actual, posicion){
     }, 1000);
 }
 
+//__________________________________________________________________________________________________________ANIMACION CARD CENTRAL
+
 card.onmouseover = () => {
     document.onmousemove = (e) => {
 
@@ -90,6 +94,168 @@ card.onmouseover = () => {
     }
 }
 
-document.querySelectorAll(".cartel").onmouseover = () => {
-    document.querySelector(".cartel").style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff00de, 0 0 25px #ff00de";
+// document.querySelectorAll(".cartel").onmouseover = () => {
+//     document.querySelector(".cartel").style.textShadow = "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff00de, 0 0 25px #ff00de";
+// }
+
+//___________________________________________________________________________________________________________________________PARALLAX
+
+window.addEventListener("scroll", () => {
+    let altoPagina = 300;
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let porcentajeScroll = scrollTop / (docHeight - winHeight);
+    let porcentajeScrollRound = Math.round(porcentajeScroll * 100);
+
+    if(porcentajeScrollRound < altoPagina) {
+        //sube card home
+        document.querySelector("#card").style.transform = `translateY(${-scrollTop}px)`;
+        //baja imagen trasera
+        document.querySelector(".fondo").style.transform = `translateY(${scrollTop}px)`;
+        //el resto de la pagina va apareciendo
+        document.querySelector(".resto-pagina").style.transform = `translateY(${-scrollTop}px)`;
+    }
+
+    //TODO: hacer que avance hasta lasiguiente pantalla asi no se queda a mitad de camino, con animacion asi no es un golpe brusco
+    
+});
+
+//_______________________________________________________________________________________________________________CARROUSEL
+
+myCarrousel();
+function myCarrousel(){
+    let imagenes = ["./images/escenas/1.png", "./images/escenas/2.png", "./images/escenas/3.png"];
+    let contador = 0;
+    let cont = setInterval(function() {adelantar();}, 9000);
+   
+    function correDiv(img){
+        img.style.animation = "carrouselElemA 3s linear 1 forwards";
+        setTimeout(function() {
+            img.style.left = "0%";//FIXME: forwards no funciona ¿?
+            img.style.animation = "carrouselElemB 3s linear 6s 1";
+            setTimeout(function() {
+                img.style.left = "-100%";
+                img.remove();
+            }, 9000) //espera a primera mas segunda
+        }, 3000) //espera a primera animacion
+        
+        //se borraban todos los div
+        // img.addEventListener("animationend", function () {
+        //     // console.log("terminada");
+        //     img.remove();
+        // })
+    }
+
+    function adelantar () {
+        if (contador + 1 == imagenes.length) {
+            contador = 0;
+        } else {
+            contador = contador + 1;
+        }
+        let elem = nuevoDiv(imagenes[contador]);
+        correDiv(elem);
+    }
+
+    function nuevoDiv(imgUrl) {  
+        let newDiv = document.createElement("div");   
+        newDiv.style.background = `url(${imgUrl})`; //ya es un string
+        newDiv.style.backgroundSize = "contain";
+        newDiv.style.backgroundRepeat = "no-repeat";
+        // añade el elemento creado y su contenido al DOM 
+        document.querySelector(".carrousel").appendChild(newDiv);  
+        return newDiv;
+    }
+}
+//________________________________________________________________________________________________COUNTDOWN
+let fechaEstreno = new Date("May 5, 2021 00:00:00").getTime();
+
+let x = setInterval(function() { //por cada segundo que pasa cambia el reloj..
+  let fechaActual = new Date().getTime();
+  let distancia = fechaEstreno - fechaActual;
+
+  let dia = Math.floor(distancia / (1000 * 60 * 60 * 24)); //floor para que no se repita valor
+  let hora = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minuto = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+  let segundo = Math.floor((distancia % (1000 * 60)) / 1000);
+
+  document.querySelector(".countdown-reloj").innerHTML = `${dia}d ${hora}h ${minuto}m ${segundo}s `;
+
+  if (fechaActual == new Date("May 5, 2021 00:00:10").getTime()) { //si llega a 10, comienzo animacion
+    crearContador();
+  }
+  if (distancia < 0) { //si llega a 0, dejo de correr el contador
+    clearInterval(x);
+  }
+}, 1000);
+
+function crearContador(){
+    let newDiv = document.createElement("div");   
+    newDiv.setAttribute("id", "countdown");
+
+    for(let i= 1; i<= 10; i++){
+        let newDiv2 = document.createElement("div");  
+        newDiv2.innerHTML = `${i}`; 
+        newDiv.appendChild(newDiv2);  
+    }
+    document.querySelector(".resto-pagina").appendChild(newDiv);  
+}
+
+//_________________________________________________________________________________PERSONAJES
+let tarjeta = document.querySelector('.personaje');
+// tarjeta.onmouseover = () => {
+//     if(!(tarjeta.classList.contains( 'clase2' ))){
+//         tarjeta.classList.add('is-flipped');
+//     }
+// }
+// tarjeta.onmouseout = () => {
+//     tarjeta.classList.remove('is-flipped');
+//   }
+//____________________________________________________________________________________FORMULARIO
+
+let form = document.querySelector(".saludos");
+
+tarjeta.onclick = () => {
+    form.classList.remove("invisible");
+    document.querySelector(".fondo-transparente").classList.remove("invisible");
+    form.style.animation = "aparicion-form 3s ease-out 1";
+}
+
+if(form){
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        let nombre = document.querySelector("#nombre").value;
+        let mail = document.querySelector("#mail").value;
+        let texto = document.querySelector("#texto").value;
+
+        form.classList.add("invisible");
+        document.querySelector(".fondo-transparente").classList.add("invisible");
+    })
+}
+
+//___________________________________________________________________________________________
+
+// let checkbox = document.querySelector(".check");
+// checkbox.addEventListener( 'change', function() {
+// if(this.checked === true) {
+//     let nav = document.querySelector("nav");
+//     nav.style.display = "block";
+//     nav.classList.add("mostrar");
+// } 
+// console.log(this.checked);
+// if(this.checked === false) {
+//     nav.style.display = "none";
+// }
+// });
+
+document.querySelector(".btn-nav").onclick = () => {
+    let nav = document.querySelector("nav");
+    if(nav.classList.contains("mostrar")){
+        nav.style.display = "none";
+        nav.classList.remove("mostrar");
+    } else {
+        nav.style.display = "block";
+        nav.classList.add("mostrar");
+    }
+    
 }
